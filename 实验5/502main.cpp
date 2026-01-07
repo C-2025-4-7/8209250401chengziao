d0d157d23b2ee567530cfc4486718bfc75fa2a3a
JiuYue-IT <dhls0620@qq.com>
2026-01-07 19:08:24 +0800

    Add files via upload

---
+++ 实验5/502main.cpp
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include"student.h"
+using namespace std;
+int main()
+{
+	Student stud;
+	stud.set_value(007, "tcg", 'm');
+	stud.display();
+	return 0;
+}
